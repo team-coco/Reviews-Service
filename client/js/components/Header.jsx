e732bc0ca07bd4827c1e327bad3bbfcf432fa28a
Mike Guin <guinzar@gmail.com>
2018-03-02 15:36:13 -0800

    Removed some minor extra code

--- client/js/components/Header.jsx
+++ client/js/components/Header.jsx
@@ -1,43 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const Header = ({reviews, fetchData }) => (
+const Header = ({ reviews }) => (
   <div className="header">
     <span className="yellow">Recommended Reviews</span> for {reviews.data.name}
   </div>
 );
 
-// const getVisibleTodos = (todos, filter) => {
-//   switch (filter) {
-//     case 'SHOW_ALL':
-//       return todos;
-//     case 'SHOW_COMPLETED':
-//       return todos.filter(
-//           t => t.completed
-//       );
-//     case 'SHOW_ACTIVE':
-//       return todos.filter(
-//           t => !t.completed
-//       );
-//   }
-// }
-
-// const mapStateToProps = (state) => {
-//   return {
-//     todos: getVisibleTodos(
-//       state.todos,
-//       state.visibilityFilter
-//     )
-//   };
-// };
 function mapStateToProps (state) {
   return {
     reviews: state.reviews
   }
 }
-const mapDispatchToProps = (dispatch) => {
-  return {
-    
-  };
-};
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default connect(mapStateToProps)(Header);
