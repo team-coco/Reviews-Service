21c35925440d2eb57e30d0622a809c1daac59c88
YZhenY <fuz3zy@gmail.com>
2018-03-30 12:52:50 -0700

    edited server to accelerate SSR

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const controller = require('./controllers');
 const app = express();
 const port = process.env.PORT || 3004;
 
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(function(req, res, next) {
@@ -18,10 +18,10 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use('/', express.static('./dist'));
+// app.use('/', express.static('./dist'));
 app.use('/main/reviews', express.static('./dist'));
 app.use('/main/reviews/ssr', express.static('./dist'));
-app.use('/api/reviews/ssr', express.static('./dist'));
+// app.use('/api/reviews/ssr', express.static('./dist'));
 
 // app.use(/\/main\/.*?\.jpg/, function (req, res) {
 //   res.status(404);
